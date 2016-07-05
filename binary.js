e401c5d27f0e20644e7848cbe80dc5c44da9d258
Yvan Sraka <yvan.sraka@gmail.com>
2016-07-05 14:49:26 +0200

    Update exercise

---
+++ binary.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the 'Binary' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at test.script.js--that should give you some hints about what is
+// expected here.
+
+'use strict';
+
+function Binary(number) {
+  this.number = number;
+}
+
+Binary.prototype.toDecimal = function () {
+  //
+  // YOUR CODE GOES HERE!
+  //
+}
+
+module.exports = Binary;
